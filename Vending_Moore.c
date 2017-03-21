2601d371dc993cd3d7ad04fe5579b3d2d234a9e9
sridhar1896 <sr510@snu.edu.in>
2017-03-21 22:19:37 +0530

    Add files via upload

---
+++ Vending_Moore.c
@@ -0,0 +1,54 @@
+//Question 3. 
+//Implemented an embedded system for a vending machine using a Mealy machine.
+//The states are :
+// 0 ---> Start state. (No output)
+// 1  output ---> coke
+// 2  output ---> coffee
+// 3  output ---> tea
+//assuming Coke, cofee and tea cost $1
+#include<stdio.h>
+int currentState=0;
+void stateTransition(char input);
+void main()
+{
+   char choice;
+   char ch = 'y';
+   while(ch=='y')
+   {
+   		currentState= 0;
+   		printf("please insert $1.\n" );
+   		printf("a. coke\n");
+		printf("b. coffee\n");
+		printf("c. Tea\n");
+		printf("Enter option.===");
+		scanf(" %c",&choice);
+		stateTransition(choice);
+		printf("\n");
+		printf("Do you wish to order another drink?(y/n)\n");
+   		scanf(" %c",&ch);
+
+   }
+
+}
+void stateTransition(char input)
+{
+	if(currentState == 0)
+	{
+		if(input=='a')
+		{
+			currentState = 1;
+			printf(" pouring one glass of  coke  ");
+		}
+		else if(input=='b')
+		{
+			currentState = 2;
+			printf(" pouring one hot cup of coffee");
+		}
+		else if(input=='c')
+		{
+			currentState = 2;
+			printf(" pouring one tea right up. ");
+		}
+	}
+
+}
